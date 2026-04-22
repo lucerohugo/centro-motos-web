5fe8bd26737c04895d064921b439667af9836b15
Hugo Lucero <h.lucero@itecriocuarto.org.ar>
2026-04-22 12:31:08 -0300

    update settings me falto una boludez

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -157,25 +157,16 @@ REST_FRAMEWORK = {
 # CORS (IMPORTANTE PARA FRONTEND)
 # ================================================================
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-
-    "https://centro-motos-web2.vercel.app",
-    "https://centro-motos-web2-hugos-projects-eb634449.vercel.app",
-
     "https://www.centromotos.com.ar",
     "https://centromotos.com.ar",
-
-    "http://138.36.237.49:8000",
-    "http://138.36.237.49",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://138.36.237.49",
-    "http://138.36.237.49:8000",
+    "https://www.centromotos.com.ar",
+    "https://centromotos.com.ar",
     "https://centro-motos-web2.vercel.app",
     "https://centro-motos-web2-hugos-projects-eb634449.vercel.app",
-    "https://api.centromotos.com.ar",  #este es el viejo:"https://api.brixsoft.com"
+    "https://api.centromotos.com.ar",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
